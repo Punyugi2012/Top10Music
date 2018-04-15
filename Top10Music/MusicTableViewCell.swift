d261bdb9c65725b7b5bea483c174029d5f154e82
Punyugi2012 <58160216@go.buu.ac.th>
2018-04-15 20:38:29 +0700

    สร้าง icon

--- Top10Music/MusicTableViewCell.swift
+++ Top10Music/MusicTableViewCell.swift
@@ -11,6 +11,7 @@ import UIKit
 class MusicTableViewCell: UITableViewCell {
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var videoImage: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -18,6 +19,7 @@ class MusicTableViewCell: UITableViewCell {
     func setValue(_ musicVideo: MusicVideo) {
         numberLabel.text = String(musicVideo.number)
         nameLabel.text = musicVideo.name
+        videoImage.image = UIImage(named: "nopic")
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
